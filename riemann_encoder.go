534d0549bac18ea628ec39cbe5fbe21224dc2ee7
cspenceiv <cspence@unomaha.edu>
2015-11-20 13:10:40 -0800

    Initial commit

---
+++ riemann_encoder.go
@@ -0,0 +1,163 @@
+/***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2012-2014
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Rob Miller (rmiller@mozilla.com)
+#   Mike Trinkala (trink@mozilla.com)
+#
+# ***** END LICENSE BLOCK *****/
+
+package heka_rieman_encoder
+
+import (
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/mozilla-services/heka/message"
+	"github.com/cspenceiv/heka-riemann-encoder/riemenc"
+)
+
+// Encoder for converting Message objects into Protocol Buffer data.
+type RiemannEncoder struct {
+	processMessageCount    int64
+	processMessageFailures int64
+	processMessageSamples  int64
+	processMessageDuration int64
+	pConfig                *PipelineConfig
+	reportLock             sync.Mutex
+	sample                 bool
+	sampleDenominator      int
+}
+
+// Heka will call this before calling any other methods to give us access to
+// the pipeline configuration.
+func (re *RiemannEncoder) SetPipelineConfig(pConfig *PipelineConfig) {
+	re.pConfig = pConfig
+}
+
+func (re *RiemannEncoder) Init(config interface{}) error {
+	re.sample = true
+	re.sampleDenominator = re.pConfig.Globals.SampleDenominator
+	return nil
+}
+
+func (re *RiemannEncoder) Encode(pack *PipelinePack) (output []byte, err error) {
+	atomic.AddInt64(&re.processMessageCount, 1)
+	var startTime time.Time
+	if re.sample {
+		startTime = time.Now()
+	}
+
+	////////////////////////////////////////////
+	// New Code
+	////////////////////////////////////////////
+	
+	// Gather pack data into riemann event struct
+	var event = &riemenc.Event{}
+	event.Time = pack.Message.Timestamp / 3e8
+	
+	switch int(pack.Message.Severity) {
+		case 0:
+			event.State = "Emergency"
+		case 1:
+			event.State = "Alert"
+		case 2:
+			event.State = "Critical"
+		case 3:
+			event.State = "Error"
+		case 4:
+			event.State = "Warning"
+		case 5:
+			event.State = "Notice"
+		case 6:
+			event.State = "Informational"
+		case 7:
+			event.State = "Debug"
+	}
+	
+	event.Service = pack.Message.Logger //Consider the use of Logger and Type
+	event.Host = pack.Message.Hostname
+	if pack.Message.Payload != nil && len(pack.Message.Payload) != 0 {
+		event.Description = pack.Message.Payload
+	}
+	//event.Tags
+	//event.Ttl
+	//event.Attributes
+	if metric, ok := pack.Message.GetFieldValue("Metric"); ok {
+		event.Metric = metric
+	}
+	
+	// End of gathering pack
+	
+	msg := &riemenc.Proto.Msg{}
+	pbevent, err := riemenc.EventToPbEvent(event)
+	if err != nil {
+		return nil, err
+	}
+	
+	msg.Events = append(msg.Events, pbevent)
+	output, err := proto.Marshal(msg)
+	if err != nil {
+		return msg, err
+	}
+	
+	////////////////////////////////////////////
+	// End New Code
+	////////////////////////////////////////////
+	
+	// Once the reimplementation of the output API is finished we should be
+	// able to just return pack.MsgBytes directly, but for now we need to copy
+	// the data to prevent problems in case the pack is zeroed and/or reused
+	// (overwriting the pack.MsgBytes memory) before we're done with it.
+	//output = make([]byte, len(pack.MsgBytes))
+	//copy(output, pack.MsgBytes)
+
+	if re.sample {
+		duration := time.Since(startTime).Nanoseconds()
+		re.reportLock.Lock()
+		re.processMessageDuration += duration
+		re.processMessageSamples++
+		re.reportLock.Unlock()
+	}
+	re.sample = 0 == rand.Intn(re.sampleDenominator)
+	return output, nil
+}
+
+func (re *RiemannEncoder) Stop() {
+	return
+}
+
+func (re *RiemannEncoder) ReportMsg(msg *message.Message) error {
+	re.reportLock.Lock()
+	defer re.reportLock.Unlock()
+
+	message.NewInt64Field(msg, "ProcessMessageCount",
+		atomic.LoadInt64(&re.processMessageCount), "count")
+	message.NewInt64Field(msg, "ProcessMessageFailures",
+		atomic.LoadInt64(&re.processMessageFailures), "count")
+	message.NewInt64Field(msg, "ProcessMessageSamples",
+		re.processMessageSamples, "count")
+
+	var tmp int64 = 0
+	if re.processMessageSamples > 0 {
+		tmp = re.processMessageDuration / re.processMessageSamples
+	}
+	message.NewInt64Field(msg, "ProcessMessageAvgDuration", tmp, "ns")
+
+	return nil
+}
+
+func init() {
+	RegisterPlugin("RiemannEncoder", func() interface{} {
+		return new(RiemannEncoder)
+	})
+}
---
+++ riemenc/riemenc.go
@@ -0,0 +1,77 @@
+package riemenc
+
+import (
+	"fmt"
+	"reflect"
+
+	pb "github.com/golang/protobuf/proto"
+	"github.com/cspenceiv/heka-riemann-encoder/riemenc/proto"
+)
+
+type Event struct {
+	Time		int64
+	State		string
+	Service		string
+	Host		string
+	Description	string
+	Tags		[]string
+	Ttl			float32
+	Attributes	map[string]string
+}
+
+// Code taken from github.com/bigdatadev/goryman
+func EventToPbEvent(event *Event) (*proto.Event, error) {
+	var e proto.Event
+	t := reflect.ValueOf(&e).Elem()
+	s := reflect.ValueOf(event).Elem()
+	typeOfEvent := s.Type()
+
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Field(i)
+		value := reflect.ValueOf(f.Interface())
+		if reflect.Zero(f.Type()) != value && f.Interface() != nil {
+			name := typeOfEvent.Field(i).Name
+			switch name {
+			case "State", "Service", "Host", "Description":
+				tmp := reflect.ValueOf(pb.String(value.String()))
+				t.FieldByName(name).Set(tmp)
+			case "Ttl":
+				tmp := reflect.ValueOf(pb.Float32(float32(value.Float())))
+				t.FieldByName(name).Set(tmp)
+			case "Time":
+				tmp := reflect.ValueOf(pb.Int64(value.Int()))
+				t.FieldByName(name).Set(tmp)
+			case "Tags":
+				tmp := reflect.ValueOf(value.Interface().([]string))
+				t.FieldByName(name).Set(tmp)
+			case "Metric":
+				switch reflect.TypeOf(f.Interface()).Kind() {
+				case reflect.Int:
+					tmp := reflect.ValueOf(pb.Int64(int64(value.Int())))
+					t.FieldByName("MetricSint64").Set(tmp)
+				case reflect.Float32:
+					tmp := reflect.ValueOf(pb.Float32(float32(value.Float())))
+					t.FieldByName("MetricF").Set(tmp)
+				case reflect.Float64:
+					tmp := reflect.ValueOf(pb.Float64(value.Float()))
+					t.FieldByName("MetricD").Set(tmp)
+				default:
+					return nil, fmt.Errorf("Metric of invalid type (type %v)",
+						reflect.TypeOf(f.Interface()).Kind())
+				}
+			case "Attributes":
+				var attrs []*proto.Attribute
+				for k, v := range value.Interface().(map[string]string) {
+					k_, v_ := k, v
+					attrs = append(attrs, &proto.Attribute{
+						Key:   &k_,
+						Value: &v_,
+					})
+				}
+				t.FieldByName(name).Set(reflect.ValueOf(attrs))
+			}
+		}
+	}
+	return &e, nil
+}
+
